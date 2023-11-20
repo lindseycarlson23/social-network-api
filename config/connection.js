08573df7cfa0d5840d5828af36fa6bd89aba5fed
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2023-11-19 19:17:35 -0600

    added database name

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
-const { connect, connection } = require('mongoose');
+const { connect, connection, connections } = require('mongoose');
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb+srv://root:root@cluster0.d4jt8qo.mongodb.net/';
+  process.env.MONGODB_URI || 'mongodb+srv://root:root@cluster0.d4jt8qo.mongodb.net/socialNetworkDb';
 
 connect(connectionString);
+console.log(connectionString);
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,23 +1,57 @@
 const express = require('express');
-const db = require('./config/connection')
+const db = require('./config/connection');
 const routes = require('./routes');
+const { MongoClient } = require('mongodb');
 
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb+srv://root:root@cluster0.d4jt8qo.mongodb.net/';
 
-const cwd = process.cwd();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-const dbName = 'socialNetworkDB';
-
 db.once('open', () => {
-    // HOW DO I 
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    
-});
-});
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+  
+// const client = new MongoClient(connectionString);
+// console.log(connectionString);
+
+// const dbName = 'socialNetworkDB';
+
+
+
+// async function connectAndOpenDb() {
+//     try {
+//         await client.connect();
+//         console.log('Connection successfuly to MongoDB');
+//         db = client.db(dbName);
+
+//         app.listen(PORT, () => {
+//             console.log(`API server running on port ${PORT}!`);
+//     });
+//     }   catch (err) {
+//         console.error('Mongo connection error: ', err.message);
+//     }  
+// }
+// connectAndOpenDb();
